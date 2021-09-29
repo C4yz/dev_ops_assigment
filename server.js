0c1b9f978eb85853b274e0939db01f160613baee
mocompres <oli@live.dk>
2021-09-29 17:20:41 +0200

    update to deployment

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
-const helmet = require('helmet') // creates headers that protect from attacks (security)
-const cors = require('cors')  // allows/disallows cross-site communication
+const helmet = require('helmet'); // creates headers that protect from attacks (security)
+const cors = require('cors');  // allows/disallows cross-site communication
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); 
 
@@ -12,19 +12,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const whitelist = ['http://localhost:3000', 'http://localhost:8080', 'https://shrouded-journey-38552.herokuapp.com']
 const corsOptions = {
   origin: function (origin, callback) {
-    console.log("** Origin of request " + origin)
+    console.log("** Origin of request " + origin);
     if (whitelist.indexOf(origin) !== -1 || !origin) {
-      console.log("Origin acceptable")
-      callback(null, true)
+      console.log("Origin acceptable");
+      callback(null, true);
     } else {
-      console.log("Origin rejected")
-      callback(new Error('Not allowed by CORS'))
+      console.log("Origin rejected");
+      callback(new Error('Not allowed by CORS'));
     }
   }
 }
-app.use(helmet())
+
+
+app.use(helmet());
 // --> Add this
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 
 app.get('/api/', (req, res) => {
   res.send({ people: 'You want to see people I assume' });
@@ -36,16 +38,17 @@ app.post('/api/', (req, res) => {
 });
 
 // --> Add this
-if (process.env.NODE_ENV === 'production') {
+//if (process.env.NODE_ENV === 'production') {
   // Serve any static files
   app.use(express.static(path.join(__dirname, 'client/build')));
 // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
+    console.log('send react funktion');
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
-}
+//}
 
 const PORT = process.env.PORT || 8080
 app.listen(PORT, (req, res) => {
-    console.log(`server listening on port: ${PORT}`)
+    console.log(`server listening on port: ${PORT}`);
   });
