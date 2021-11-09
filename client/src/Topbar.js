91bd5a11eb857e99f7a32c3522aee86445d2a95f
unknown <s143861@student.dtu.dk>
2021-11-09 09:16:32 +0100

    Fixed bug in topbar

--- client/src/Topbar.js
+++ client/src/Topbar.js
@@ -11,8 +11,7 @@ function Topbar(props){
 
     const onClickHandler = event =>  {
         console.log("this happens" + event);
-        props.store.changeStore(event);
-        history.push(`/courses/${event}/2`);
+        history.push(`/courses/${event}/day1`);
 
     };
 
