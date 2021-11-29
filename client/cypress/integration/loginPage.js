b358fb9ecd38680833839054182c6f2321978b0c
Thomas <thomas8@hohnen.dk>
2021-11-29 21:30:09 +0100

    working on login test

--- client/cypress/integration/loginPage.js
+++ client/cypress/integration/loginPage.js
@@ -4,13 +4,21 @@ describe("Se if the landing page renders", () => {
         cy.get("#container").should("exist")
     });
 
-    /*it("Should login when button is clicked", () => {
-        cy.contains("Click here to login").click()
+    it("Should login when button is clicked", () => {
+        cy.contains("Click here to login").should("exist")
 
-        /*cy.wait(4000)
-        cy.get("input[type=email]").type("s195455")
+        cy.request({
+            url:"http://localhost:5000/login"
+            //followRedirect: true
+        }).then((response) => {
+            expect(response.status).to.eq(200)
+            expect(response.redirectedToUrl).to.eq("http://localhost:5000/redirect")
+        })
+
+        //cy.request('https://auth.dtu.dk/dtu/?service=http://localhost:5000/redirect')
+        /*cy.get("input[type=email]").type("s195455")
         cy.get("input[type=password]").type("Yhp84kxj")
         cy.contains("Log på").click()
-        cy.get("#boardContainer").should("exist")
-    });*/
+        cy.get("#boardContainer").should("exist")*/
+    });
 });
