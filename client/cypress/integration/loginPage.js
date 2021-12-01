bbcb8d0de323eb92197788f567a2443d2e8b360a
Thomas Hohnen <55696172+C4yz@users.noreply.github.com>
2021-12-01 10:20:19 +0100

    wokring on JSON env file

--- client/cypress/integration/loginPage.js
+++ client/cypress/integration/loginPage.js
@@ -1,19 +1,30 @@
+import { sign } from 'jsonwebtoken';
+
 describe("Se if the landing page renders", () => {
+
     it("Should render correctly", () =>{
         cy.visit("/")
         cy.get("#container").should("exist")
     });
 
     it("Login button should exist", () => {
-        cy.contains("Click here to login").click()
+        //cy.contains("Click here to login").should('exist')
 
-        //cy.visit("https://auth.dtu.dk/dtu/?service=http://localhost:5000/redirect")
+
 
     });
 
     it("Should validate the token if correct", () => {
 
-        cy.request("http://localhost:5000/testAPI?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdHVkZW50bnVtYmVyIjoiczE5NTQ1NSIsImlhdCI6MTYzODI3NzY0OSwiZXhwIjoxNjM4Mjg0ODQ5fQ.gs9XCNyUP7ttn_j-st3D0ao7FtFcGtXknOhl3IULgoY")
+        const token = sign(
+            {studentnumber: "s195455"},
+            Cypress.env("JWT_TOKEN"),
+            {
+                expiresIn: "2h"
+            }
+        )
+
+        cy.request("http://localhost:5000/testAPI?token="+token)
         .then((response) => {
             expect(response.status).to.eq(200)
         })
