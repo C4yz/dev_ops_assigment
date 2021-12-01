0abb6b0aaa4776a9c0f7071e76b5761c1c821547
Thomas <thomas8@hohnen.dk>
2021-12-01 16:14:41 +0100

    getting ready to merge main test

--- client/cypress/integration/loginPage.js
+++ client/cypress/integration/loginPage.js
@@ -14,25 +14,7 @@ describe("Se if the landing page renders", () => {
 
     });
 
-    it("Should validate the token if correct", () => {
-
-        const token = sign(
-            {studentnumber: "s195455"},
-            Cypress.env("JWT_TOKEN"),
-            {
-                expiresIn: "2h"
-            }
-        )
-        //verify(token, Cypress.env("JWT_TOKEN"))
-
-        cy.request("http://localhost:5000/testAPI?token="+token)
-        .then((response) => {
-            expect(response.status).to.eq(200)
-        })
-
-    });
-
-    it("Should not validte the token", () => {
+    it("Should not validte if the token is wrong", () => {
         cy.request({
             url:"http://localhost:5000/testAPI?token=HuberBubber",
             failOnStatusCode: false
